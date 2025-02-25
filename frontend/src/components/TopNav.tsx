0ec59faa3a2bf27153c1d9b7cd6e263b08ae6a5d
Lachlan Stavrevski <locky.stav@gmail.com>
2025-02-26 01:27:17 +1100

    Add proper menu to the side drawer

--- frontend/src/components/TopNav.tsx
+++ frontend/src/components/TopNav.tsx
@@ -4,43 +4,59 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
+import { Box, Drawer, ListItem, ListItemButton, ListItemText, Divider } from '@mui/material';
+import { AccountCircleOutlined, Inbox } from '@mui/icons-material';
 
 export default function TopNav() {
-  const [drawerOpen, setDrawerOpen] = useState(false);
+	const [drawerOpen, setDrawerOpen] = useState(false);
+	const toggleDrawer = (open: boolean) => () => {
+		setDrawerOpen(open);
+	};
 
-  const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
-    if (event.type === 'keydown' && ((event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift')) {
-      return;
-    }
-    setDrawerOpen(open);
-  };
+	const DrawerInside = (
+		<Box sx={{ width: '45vw'}}>
+			<Typography variant="h6" align="center" sx={{ my: 2 }}>
+				Manage Trip
+			</Typography>
+			<Divider variant='middle' sx={{ bgcolor: 'grey' }} />
+			<ListItem disablePadding sx={{ justifyContent: 'flex-end' }}>
+				<ListItemButton>
+					<ListItemText primary="Edit Trip" sx={{ textAlign: 'right' }} />
+				</ListItemButton>
+			</ListItem>
+			<ListItem disablePadding sx={{ justifyContent: 'flex-end' }}>
+				<ListItemButton>
+					<ListItemText primary="Travelers" sx={{ textAlign: 'right' }} />
+				</ListItemButton>
+			</ListItem>
+		</Box>
+	)
 
   return (
-    <>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer(true)}>
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6">
-            Trip Planner
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
-        <List>
-          <ListItem >
-            <ListItemText primary="Edit Trip" />
-          </ListItem>
-          <ListItem >
-            <ListItemText primary="Travelers" />
-          </ListItem>
-        </List>
-      </Drawer>
-    </>
+	<>
+	<AppBar sx={{ backgroundColor: 'white', color: 'black' }} position="static">
+		<Toolbar>
+			<IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer(true)}>
+				<MenuIcon />
+			</IconButton>
+			<Typography variant="h5" sx={{ flexGrow: 1 }}>
+				
+			</Typography>
+			<IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                color="inherit"
+                sx={{ fontSize: 30 }} // Increase the size of the icon
+              >
+                <AccountCircleOutlined fontSize="inherit" />
+              </IconButton>
+		</Toolbar>
+	</AppBar>
+	<Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
+		{DrawerInside}
+	</Drawer>
+</>
   );
 }

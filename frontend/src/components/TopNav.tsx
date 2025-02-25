04dd92ec221b37a71f3813b9e6a28fb9a5efaa9f
Lachlan Stavrevski <locky.stav@gmail.com>
2025-02-26 00:26:01 +1100

    Implementation of basic top navbar

---
+++ frontend/src/components/TopNav.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+
+export default function TopNav() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+    if (event.type === 'keydown' && ((event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift')) {
+      return;
+    }
+    setDrawerOpen(open);
+  };
+
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer(true)}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">
+            Trip Planner
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
+        <List>
+          <ListItem >
+            <ListItemText primary="Edit Trip" />
+          </ListItem>
+          <ListItem >
+            <ListItemText primary="Travelers" />
+          </ListItem>
+        </List>
+      </Drawer>
+    </>
+  );
+}
--- frontend/src/layouts/MainScreen.tsx
+++ frontend/src/layouts/MainScreen.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router";
 import BottomNav from "../components/BottomNav";
+import TopNav from "../components/TopNav";
 
 export default function MainScreenLayout() {
 	return (
 		<>
+			<TopNav />
 			<Outlet />
 			<BottomNav />
 		</>

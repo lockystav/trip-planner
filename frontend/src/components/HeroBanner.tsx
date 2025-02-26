78c55bea6223cfcb101e41e91bd9b0a8025b6ecd
Lachlan Stavrevski <locky.stav@gmail.com>
2025-02-27 01:35:31 +1100

    Hero Banner component on the home page

---
+++ frontend/src/components/HeroBanner.tsx
@@ -0,0 +1,83 @@
+import { ArrowDropDown } from '@mui/icons-material';
+import React, { useState } from 'react';
+
+const HeroBanner: React.FC = () => {
+  const [status, setStatus] = useState('going');
+
+  const handleStatusChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setStatus(event.target.value);
+  };
+
+  const getStatusColor = () => {
+    switch (status) {
+      case 'going':
+        return ['limegreen', 'white'];
+      case 'maybe':
+        return ['yellow', 'black'];
+      case 'not-going':
+        return ['red', 'white'];
+      default:
+        return ['grey', 'white'];
+    }
+  };
+
+  return (
+    <div style={styles.container}>
+      <h1 style={styles.title}>Trip Planner</h1>
+      <div style={{ ...styles.dropdownContainer, backgroundColor: getStatusColor()[0] }}>
+        <ArrowDropDown sx={{color:getStatusColor()[1],  marginLeft: '-0.35rem'}}/>
+        <select
+          value={status}
+          onChange={handleStatusChange}
+          style={{ ...styles.dropdown, color: getStatusColor()[1] }}
+        >
+          <option value="going">Going</option>
+          <option value="not-going">Not Going</option>
+          <option value="maybe">Maybe</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+const styles: { [key: string]: React.CSSProperties } = {
+  container: {
+    height: '25vh',
+    width: '100%',
+    backgroundColor: 'lightgrey',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+},
+title: {
+	color: 'grey',
+    fontSize: '2rem',
+    marginBottom: '1rem',
+},
+dropdownContainer: {
+	display: 'flex',
+    alignItems: 'center',
+    padding: '0.5rem 1rem',
+    borderRadius: '100px',
+    marginTop: '1rem',
+},
+arrow: {
+	width: 0,
+    height: 0,
+    borderLeft: '6px solid transparent',
+    borderRight: '6px solid transparent',
+    borderTop: '6px solid white',
+    marginRight: '0.5rem',
+},
+dropdown: {
+	border: 'none',
+    background: 'none',
+	textAlign: 'center',
+    fontSize: '1rem',
+    appearance: 'none',
+    padding: 0,
+},
+};
+
+export default HeroBanner;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useNavContext } from '../components/contexts/NavContext';
+import HeroBanner from '../components/HeroBanner';
 
 const Home: React.FC = () => {
 	const { setShowDrawer, setShowAccount } = useNavContext();
@@ -11,6 +12,7 @@ const Home: React.FC = () => {
 
     return (
         <div>
+            <HeroBanner />
             <h1>Welcome to the Trip Planner</h1>
             <p>This is the home page. Start planning your trip now!</p>
         </div>

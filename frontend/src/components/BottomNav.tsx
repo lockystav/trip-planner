149ead22cc4768f38bcaedc40c6cd90e0ae202e3
Lachlan Stavrevski <locky.stav@gmail.com>
2025-03-03 22:45:50 +1100

    Update BottomNav to reflect current route using useLocation; improve state management with useEffect

--- frontend/src/components/BottomNav.tsx
+++ frontend/src/components/BottomNav.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { Link } from 'react-router';
+import { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router';
 
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
@@ -10,46 +10,51 @@ import HotelIcon from '@mui/icons-material/Hotel';
 import PollIcon from '@mui/icons-material/Poll';
 
 export default function BottomNav() {
-  const [value, setValue] = useState('Home');
+	const location = useLocation();
+  	const [value, setValue] = useState('');
 
-  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-	setValue(newValue);
-  }
+	const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+		setValue(newValue);
+	}
 
-  return (
-	<BottomNavigation
-		value={value}
-		onChange={handleChange}
-		style={{ position: 'fixed', bottom: 0, width: '100%', borderTop: '0.5px solid #ccc' }}
-	>
-		<BottomNavigationAction 
-			label="Home"
-			value="home"
-			icon={<HomeIcon />}
-			component={Link}
-			to=''
-		/>
-		<BottomNavigationAction
-			label="Board"
-			value="board"
-			icon={<DashboardIcon />}
-			component={Link}
-			to="board"
-		/>
-		<BottomNavigationAction
-			label="Accomodation"
-			value="accomodation"
-			icon={<HotelIcon />}
-			component={Link}
-			to="accomodation"
-		/>
-		<BottomNavigationAction
-			label="Polls"
-			value="polls"
-			icon={<PollIcon />}
-			component={Link}
-			to="polls"
-		/>
-	</BottomNavigation>
-  );
+	useEffect(() => {
+		setValue(location.pathname === '/' ? 'home' : location.pathname.substring(1));
+	  }, [location]);
+
+	return (
+		<BottomNavigation
+			value={value}
+			onChange={handleChange}
+			style={{ position: 'fixed', bottom: 0, width: '100%', borderTop: '0.5px solid #ccc' }}
+		>
+			<BottomNavigationAction 
+				label="Home"
+				value="home"
+				icon={<HomeIcon />}
+				component={Link}
+				to=''
+			/>
+			<BottomNavigationAction
+				label="Board"
+				value="board"
+				icon={<DashboardIcon />}
+				component={Link}
+				to="board"
+			/>
+			<BottomNavigationAction
+				label="Accomodation"
+				value="accomodation"
+				icon={<HotelIcon />}
+				component={Link}
+				to="accomodation"
+			/>
+			<BottomNavigationAction
+				label="Polls"
+				value="polls"
+				icon={<PollIcon />}
+				component={Link}
+				to="polls"
+			/>
+		</BottomNavigation>
+	);
 }

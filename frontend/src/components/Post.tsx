606250651ea8f6103c0a2622515dc7f3738b9b7f
Lachlan Stavrevski <locky.stav@gmail.com>
2025-03-04 00:02:25 +1100

    Refactor the post component to take in data and display the data pulled in

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { IconButton, Divider } from '@mui/material';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import CommentIcon from '@mui/icons-material/Comment';
 import { AccountCircleOutlined } from '@mui/icons-material';
 
-const Post: React.FC = () => {
+interface PostProps {
+  post: {
+    content: string;
+    likes: number;
+    comments: string[];
+    liked: boolean;
+  };
+}
+
+const Post: React.FC<PostProps> = ({ post }) => {
+  const { content, likes, comments, liked } = post;
+
   return (
-    <div className='my-3 max-w-[1000px] w-[95%] p-[10px] border border-gray-300 rounded-lg flex flex-col justify-between'>
+    <div className='my-3 max-w-[1000px] w-[95%] p-[10px] border border-gray-300 rounded-xl flex flex-col justify-between'>
       <div className='flex justify-between items-center'>
         <AccountCircleOutlined fontSize='large' className='text-gray-500' />
         <IconButton className='p-1'>
@@ -15,19 +27,22 @@ const Post: React.FC = () => {
         </IconButton>
       </div>
       <div className='flex-1 p-2 overflow-auto'>
-        {/* Post content goes here */}
-        <p className='text-base'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, pulvinar facilisis justo mollis, auctor consequat urna.
-        </p>
+        <p className='text-base'>{content}</p>
       </div>
       <Divider />
       <div className='flex justify-between items-center pt-1 px-[10%]'>
-        <IconButton>
-          <FavoriteBorderIcon />
-        </IconButton>
-        <IconButton>
-          <CommentIcon />
-        </IconButton>
+        <div className='flex items-center'>
+          <IconButton>
+            {liked ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+          </IconButton>
+          <span className='ml-1 text-lg w-6'>{likes > 0 ? likes : ''}</span>
+        </div>
+        <div className='flex items-center'>
+          <IconButton>
+            <CommentIcon />
+          </IconButton>
+          <span className='ml-1 text-lg w-6'>{comments.length > 0 ? comments.length : ''}</span>
+        </div>
       </div>
     </div>
   );
--- frontend/src/pages/Board.tsx
+++ frontend/src/pages/Board.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Post from '../components/Post';
+import { posts } from '../data/posts';
 
 const Board: React.FC = () => {
   return (
-    <div className="pt-5 px-[10%] flex justify-between flex-col items-center">
-      <Post />
+    <div className="pt-5 px-[5%] flex justify-between flex-col items-center">
+      {posts.map(post => (
+        <Post key={post.id} post={post} />
+      ))}
     </div>
   );
 };

302d69cee80d3d7c36a7e43cf780b5babad79852
Lachlan Stavrevski <locky.stav@gmail.com>
2025-03-03 23:59:23 +1100

    Add posts data structure with sample posts, likes, and comments

---
+++ frontend/src/data/posts.ts
@@ -0,0 +1,37 @@
+export const posts = [
+  {
+    id: 1,
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, pulvinar facilisis justo mollis, auctor consequat urna.",
+    likes: 10,
+    comments: [
+      "Great post!",
+      "Thanks for sharing.",
+      "Very informative."
+    ],
+    liked: true
+  },
+  {
+    id: 2,
+    content: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    likes: 5,
+    comments: [],
+    liked: false
+  },
+  {
+    id: 3,
+    content: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+    likes: 0,
+    comments: [
+      "Interesting perspective.",
+      "I disagree with this."
+    ],
+    liked: false
+  },
+  {
+    id: 4,
+    content: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    likes: 8,
+    comments: [],
+    liked: true
+  }
+];
